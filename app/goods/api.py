efbfcac07702edc4c142436df6c16e6afea384a8
AlexDyakonov <shram.monolit@mail.ru>
2024-09-22 07:33:32 +0300

    Added endpoint that counts NEW trigger

--- app/goods/api.py
+++ app/goods/api.py
@@ -1,7 +1,9 @@
+import pandas as pd
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db
+from app.model import predict
 
 from . import crud
 from .schemas import Goods, GoodsCreate
@@ -29,10 +31,35 @@ def read_goods_by_shelve(shelve: str, db: Session = Depends(get_db)):
 
 
 @router.put("/{sku_id}/amount", response_model=Goods)
-def update_goods_amount(sku_id: str, new_amount: int, db: Session = Depends(get_db)):
-    db_goods = crud.update_goods_amount(db=db, sku_id=sku_id, new_amount=new_amount)
+def update_goods_amount_and_trigger(
+    sku_id: str, new_amount: int, db: Session = Depends(get_db)
+):
+    db_goods = crud.get_goods_by_sku(db=db, sku_id=sku_id)
+
     if db_goods is None:
         raise HTTPException(status_code=404, detail="Goods not found")
+
+    input_data = pd.DataFrame(
+        {
+            "sku_id": [db_goods.sku_id],
+            "name": [db_goods.name],
+            "category": [db_goods.category],
+            "amount": [new_amount],
+            "avg_cart": [db_goods.avg_cart],
+            "t_expiery": [db_goods.t_expiery],
+            "stocktaking_time": [db_goods.stocktaking_time],
+            "dt": [db_goods.dt],
+        }
+    )
+
+    predicted_trigger = predict(input_data).data[0]
+
+    db_goods.amount = new_amount
+    db_goods.trigger = predicted_trigger
+
+    db.commit()
+    db.refresh(db_goods)
+
     return db_goods
 
 
--- app/model.py
+++ app/model.py
@@ -4,7 +4,9 @@ import joblib
 
 
 def load_model():
-    model = joblib.load("./models/fruit_model.pkl")
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(base_dir, "models", "fruit_model.pkl")
+    model = joblib.load(model_path)
     return model
 
 
@@ -12,5 +14,5 @@ model = load_model()
 
 
 def predict(input_data):
-    prediction = model.predict([input_data])
-    return prediction[0]
+    prediction = model.predict(input_data)
+    return prediction
