ebbc963f1136496db3aa79fe4194885e58edaac7
AlexDyakonov <shram.monolit@mail.ru>
2024-09-22 08:39:08 +0300

    Turn down CORS

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import database as db
 from .goods.api import router as goods_router
@@ -6,5 +7,14 @@ from .goods.api import router as goods_router
 db.Base.metadata.create_all(bind=db.engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(prefix="/goods", router=goods_router)
